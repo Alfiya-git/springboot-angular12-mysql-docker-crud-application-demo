c40c251073b147233250c91e5536471e6bba30b4
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-09-09 13:08:45 +0200

    Updating frontend module: added app-routing.module, some components

---
+++ angular12-crud-application-demo/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'employee', pathMatch: 'full' },
+    { path: 'add', component: CreateEmployeeComponent },
+    { path: 'employees', component: EmployeeListComponent },
+    { path: 'update/:id', component: UpdateEmployeeComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- angular12-crud-application-demo/src/app/app.module.ts
+++ angular12-crud-application-demo/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
